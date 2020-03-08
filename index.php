d79e313af2eb9ca56808781f0a15081919461552
OliverZott <zott_oliver@web.de>
2020-03-08 09:09:10 +0100

    index adapted to repair status (constructor example)

--- index.php
+++ index.php
@@ -62,15 +62,23 @@ if (isset($_GET['error'])) {
                         <th>Weapon Power</th>
                         <th>Jedi Factor</th>
                         <th>Strength</th>
+                        <th>Status</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php foreach ($ships as $ship): ?>
                         <tr>
                             <td><?php echo $ship->getName(); ?></td>
-                            <td><?php echo $ship->weaponPower; ?></td>
-                            <td><?php echo $ship->jediPower; ?></td>
-                            <td><?php echo $ship->strength; ?></td>
+                            <td><?php echo $ship->getWeaponPower(); ?></td>
+                            <td><?php echo $ship->getJediPower(); ?></td>
+                            <td><?php echo $ship->getStrength(); ?></td>
+                            <td>
+                                <?php if ($ship->isFunctional()): ?>
+                                    <i class="fa fa-sun-o"></i>
+                                <?php else: ?>
+                                    <i class="fa fa-cloud"></i>
+                                <?php endif; ?>
+                            </td>
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
@@ -84,7 +92,9 @@ if (isset($_GET['error'])) {
                         <select class="center-block form-control btn drp-dwn-width btn-default dropdown-toggle" name="ship1_name">
                             <option value="">Choose a Ship</option>
                             <?php foreach ($ships as $key => $ship): ?>
-                                <option value="<?php echo $key; ?>"><?php echo $ship->getNameAndSpecs(); ?></option>
+                                <?php if($ship->isFunctional()): ?>
+                                    <option value="<?php echo $key; ?>"><?php echo $ship->getNameAndSpecs(); ?></option>
+                                <?php endif; ?>
                             <?php endforeach; ?>
                         </select>
                         <br>
@@ -94,7 +104,9 @@ if (isset($_GET['error'])) {
                         <select class="center-block form-control btn drp-dwn-width btn-default dropdown-toggle" name="ship2_name">
                             <option value="">Choose a Ship</option>
                             <?php foreach ($ships as $key => $ship): ?>
-                                <option value="<?php echo $key; ?>"><?php echo $ship->getNameAndSpecs(true); ?></option>
+                                <?php if($ship->isFunctional()): ?>
+                                    <option value="<?php echo $key; ?>"><?php echo $ship->getNameAndSpecs(true); ?></option>
+                                <?php endif; ?>
                             <?php endforeach; ?>
                         </select>
                         <br>
