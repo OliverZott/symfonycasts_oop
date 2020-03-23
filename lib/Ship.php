b5f33ce94b4703c28d221f7268429b34a3dc4ea9
OliverZott <zott_oliver@web.de>
2020-03-23 17:58:56 +0100

    course 2 chapter 6: new private attribute for database integration (ID)

--- lib/Ship.php
+++ lib/Ship.php
@@ -2,6 +2,7 @@
 
 class Ship {
 
+    private int $id;
     private string $name = 'DefaultName';
     private int $weaponPower = 0;
     private int $jediPower = 0;
@@ -101,6 +102,23 @@ class Ship {
             $this->strength = $strength;
         }
     }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
 
 }
 
