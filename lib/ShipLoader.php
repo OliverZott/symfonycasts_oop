ffaf546f8e13722e38fd0f18f8ed6b1814761274
OliverZott <zott_oliver@web.de>
2020-03-23 17:58:28 +0100

    course 2 chapter 6: get ship from DATABASE; added functions for ship-query and get Ship by id

--- lib/ShipLoader.php
+++ lib/ShipLoader.php
@@ -2,52 +2,76 @@
 /**
  * Class ShipLoader
  *
- * Service Class to create Ships
+ * Service Class to create Array of Ships
+ *
+ * - Course 2 - Chapter 6, 7
+ * - USE id instead of ship-name in index.php
+ *
  */
 
 
-class ShipLoader {
+class ShipLoader
+{
 
     public function getShips()
     {
+        /**
+         * Course2 - Chapter6/7
+         * - get Ships from Database
+         * - generate Ship Object from Database array
+         * - store all Ship objects in an array
+         *
+         */
+        $shipsArray = $this->queryForShips();
 
         $ships = array();
 
-        $ship = new Ship('Jedi Starfighter');
-        $ship->setWeaponPower(5);
-        $ship->setJediPower(15);
-        $ship->setStrength(30);
-
-        $ship2 = new Ship('CloakShape Fighter');
-        $ship2->setWeaponPower(2);
-        $ship2->setJediPower(2);
-        // try catch cause exception in declaration
-        try {
-            $ship2->setStrength(70);
-        } catch (Exception $e) {
+        foreach ($shipsArray as $ship)
+        {
+            $shipData = new Ship($ship['name']);
+            $shipData->setId($ship['id']);
+            $shipData->setWeaponPower($ship['weapon_power']);
+            $shipData->setJediPower($ship['jedi_factor']);
+            $shipData->setStrength($ship['strength']);
+            $ships[] = $shipData;
         }
-
-        $ship3 = new Ship('Super Star Destroyer');
-        $ship3->setWeaponPower(70);
-        $ship3->setJediPower(0);
-        $ship3->setStrength(500);
-
-        $ship4 = new Ship('RZ-1 A-wing interceptor');
-        $ship4->setWeaponPower(4);
-        $ship4->setJediPower(4);
-        $ship4->setStrength(50);
-
-
-        // without specific key:
-        // $ships[] = $ship;
-
-        $ships['starfighter'] = $ship;
-        $ships['CloakShape Fighter'] = $ship2;
-        $ships['Super Star Destroyer'] = $ship3;
-        $ships['RZ-1 A-wing interceptor'] = $ship4;
 
         return $ships;
 
+    }
+
+
+    public function getShipById($id)
+    {
+        $pdo = new PDO('mysql:host=localhost;dbname=oo_battle', 'root');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // prepare... normal query but prevents SQL injection attacks
+        // https://stackoverflow.com/questions/60174/how-can-i-prevent-sql-injection-in-php
+
+        $statement = $pdo->prepare('SELECT * FROM ship WHERE id = :id');
+        $statement->execute(array('id' => $id));
+        $shipsArray = $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+    private function queryForShips()
+    {
+        /**
+         * Course 2 - Chapter 6
+         * Use Database for ShipLoader!
+         *
+         * Idea: use "small private function" to have:
+         *  - a proper name for code parts
+         *  - easier reuse and maintain code
+         */
+
+        $pdo = new PDO('mysql:host=localhost;dbname=oo_battle', 'root');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $statement = $pdo->prepare('SELECT * FROM ship');
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
 }
